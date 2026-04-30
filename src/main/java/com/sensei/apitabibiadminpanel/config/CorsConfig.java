8883d967d9b300097ebca69ecae80613712fb7e5
yahyaSensei <yahyahisham274@gmail.com>
2026-04-30 15:11:46 +0300

    update connection string

---
+++ src/main/java/com/sensei/apitabibiadminpanel/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.sensei.apitabibiadminpanel.config; // غيرها لو سميت الفولدر حاجة تانية
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
